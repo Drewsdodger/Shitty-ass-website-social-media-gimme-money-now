7dcb53ffaaa9e5763e178252315746f9265faa6d
Drewsdodger <67015438+Drewsdodger@users.noreply.github.com>
2023-07-13 23:37:31 +0100

    nut
    nut master

---
+++ script.js
@@ -0,0 +1,45 @@
+// An array to store the posts
+var posts = [];
+
+// Function to handle form submission
+function handleFormSubmit(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  var postContent = document.getElementById('post-content').value; // Get the post content
+
+  if (postContent) {
+    var post = {
+      content: postContent,
+      timestamp: new Date()
+    };
+
+    posts.push(post); // Add the post to the array
+
+    displayPosts(); // Display the updated post list
+
+    document.getElementById('post-content').value = ''; // Clear the input field
+  }
+}
+
+// Function to display the posts
+function displayPosts() {
+  var postList = document.getElementById('post-list');
+  postList.innerHTML = ''; // Clear the post list
+
+  for (var i = posts.length - 1; i >= 0; i--) {
+    var post = posts[i];
+
+    var postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.innerHTML = '<p>' + post.content + '</p><small>' + post.timestamp + '</small>';
+
+    postList.appendChild(postElement);
+  }
+}
+
+// Add event listener to the form submission
+var form = document.getElementById('post-form');
+form.addEventListener('submit', handleFormSubmit);
+
+// Initial display of posts
+displayPosts();
